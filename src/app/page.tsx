79f5a8715c7a4e25821058b7379304ebc1386db4
Christine Bautista <c.bautista2503@gmail.com>
2026-09-10 18:12:07 -0700

    added binder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Binder from "@/components/binder/Binder"
+
 export default function Home() {
   return (
     <main>
       <h1>Pokémon Card Binder</h1>
       <p>Welcome to my collection.</p>
+      <Binder />
     </main>
   );
 }
---
+++ src/components/binder/Binder.tsx
@@ -0,0 +1,8 @@
+export default function Binder() {
+  return (
+    <div>
+      <h2>Binder page</h2>
+      <p>wip come back later :D</p>
+    </div>
+  )
+}
